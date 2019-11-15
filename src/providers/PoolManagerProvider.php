23f3345d1d2a46be8cc88f077a6698f696239061
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2019-11-14 22:48:35 -0800

    Rework to use dir()

--- src/providers/PoolManagerProvider.php
+++ src/providers/PoolManagerProvider.php
@@ -29,34 +29,36 @@ class PoolManagerProvider implements Hiraeth\Provider
 	public function __invoke(object $instance, Hiraeth\Application $app): object
 	{
 		$ephemeral = new Stash\Driver\Ephemeral();
+		$defaults  = [
+			'class'    => NULL,
+			'disabled' => FALSE,
+			'options'  => array()
+		];
 
-		foreach ($app->getConfig('*', 'cache', []) as $path => $config) {
-			if (isset($config['class'])) {
-				$name    = basename($path);
-				$options = $config['options'] ?? [];
-				$drivers = array();
-
-				if ($instance->has($name)) {
-					throw new RuntimeException(sprintf(
-						'Cannot configure cache "%s", another cache already exists with that name',
-						$name
-					));
-				}
-
-				if (!$config['disabled'] ?? TRUE) {
-					if (isset($options['path'])) {
-						$options['path'] = $app->getDirectory($options['path'], TRUE)->getRealPath();
-					}
-
-					$drivers[] = new $config['class']($options);
-				}
-
-				$stack  = new Stash\Driver\Composite(['drivers' => $drivers + [$ephemeral]]);
-				$pool   = new Stash\Pool($stack);
-
-				$instance->add($name, $pool);
-				$pool->setNamespace($name);
+		foreach ($app->getConfig('*', 'cache', $defaults) as $path => $config) {
+			if (!$config['class']) {
+				continue;
 			}
+
+			$name    = basename($path);
+			$drivers = array();
+
+			if ($instance->has($name)) {
+				throw new RuntimeException(sprintf(
+					'Cannot configure cache "%s", another cache already exists with that name',
+					$name
+				));
+			}
+
+			if (!$config['disabled']) {
+				$drivers[] = new $config['class']($config['options']);
+			}
+
+			$stack  = new Stash\Driver\Composite(['drivers' => $drivers + [$ephemeral]]);
+			$pool   = new Stash\Pool($stack);
+
+			$instance->add($name, $pool);
+			$pool->setNamespace($name);
 		}
 
 		return $instance;
